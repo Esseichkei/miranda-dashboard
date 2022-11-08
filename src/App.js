0d8ed7a624d78d4b75b4b138d1f5cabde508e2a4
Andres 'SHK' F. P <andrs.fp@gmail.com>
2022-11-08 09:40:15 +0100

    adds basic routing to every main component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard } from "./components/dashboard/main";
+import { Login } from "./components/login/main";
+import { Bookings } from "./components/bookings/main";
+import { Rooms } from "./components/rooms/main";
+import { Users } from "./components/users/main";
+import { Contact } from "./components/contact/main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/bookings" element={<Bookings/>} />
+        <Route path="/bookings/:id" element={<Bookings/>} />
+        <Route path="/rooms" element={<Rooms/>} />
+        <Route path="/rooms/:id" element={<Rooms/>} />
+        <Route path="/users" element={<Users/>} />
+        <Route path="/users/:id" element={<Users/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/contact/:id" element={<Contact/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/login/main.js
@@ -0,0 +1,3 @@
+export function Login(props) {
+    return (<h1>Login!</h1>);
+}
