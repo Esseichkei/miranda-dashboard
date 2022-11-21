2f7fa19aef4ee4e4496717fe753e69b94bd46330
Andres 'SHK' F. P <andrs.fp@gmail.com>
2022-11-21 13:59:57 +0100

    adds the rest of the links to the Rooms sidebar

--- src/components/rooms/Rooms.js
+++ src/components/rooms/Rooms.js
@@ -2,6 +2,9 @@ import { HeaderDiv, HeaderLeftDiv, MainDiv, SidebarDiv, SidebarToggle, HeaderRig
 import { useState } from "react";
 import { ReactComponent as GridSvg } from "../../img/grid-interface-layout-icon.svg";
 import { ReactComponent as KeySvg } from "../../img/key-line-icon.svg";
+import { ReactComponent as CalendarSvg} from "../../img/schedule-calendar-icon.svg";
+import { ReactComponent as AvatarSvg} from "../../img/male-icon.svg";
+import { ReactComponent as JigsawSvg} from "../../img/extension-icon.svg";
 
 export function Rooms(props) {
     const [sidebarShow, setSidebarShow] = useState(true);
@@ -35,6 +38,18 @@ export function Rooms(props) {
                 <SidebarNavLink to="/rooms" end>
                     <SidebarIconWrapper><KeySvg /></SidebarIconWrapper>
                     <h3>Rooms</h3>
+                </SidebarNavLink>
+                <SidebarNavLink to="/bookings" end>
+                    <SidebarIconWrapper><CalendarSvg /></SidebarIconWrapper>
+                    <h3>Bookings</h3>
+                </SidebarNavLink>
+                <SidebarNavLink to="/users" end>
+                    <SidebarIconWrapper><AvatarSvg /></SidebarIconWrapper>
+                    <h3>Users</h3>
+                </SidebarNavLink>
+                <SidebarNavLink to="/contact" end>
+                    <SidebarIconWrapper><JigsawSvg /></SidebarIconWrapper>
+                    <h3>Contact</h3>
                 </SidebarNavLink>
             </SidebarDiv>
             <h1>Rooms!</h1>
